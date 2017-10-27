4c37e654522fcc6381743f9e916c4ae8ae0dd174
royernst <roy.e31@hotmail.com>
2017-10-27 10:38:04 -0400

    set up list class

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,5 +15,10 @@ namespace ToDo.Models
         public DateTime DueDate { get; set; }
         public string Details { get; set; }
         public bool IsDone { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        //Keeps a reference to whichever list the item is associated with
+        public virtual List List { get; set; }
     }
 }
---
+++ ToDo/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        public string ListName { get; set; }
+        
+        public virtual ICollection<Item> Item { get; set; }
+
+    }
+}
