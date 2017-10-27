a3e6bf5eb5609c6d992de8e3358d0bc3c73e7c06
royernst <roy.e31@hotmail.com>
2017-10-27 10:25:04 -0400

    set up item model

---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Details { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
