fbc09d4a10bfe8b8dad8660c41eb4c1f8960a0bf
royernst <roy.e31@hotmail.com>
2017-10-27 10:40:37 -0400

    Added item and list controllers

---
+++ ToDo/Models/ToDoContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class ToDoContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ToDoContext() : base("name=ToDoContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ToDo.Models.Item> Items { get; set; }
+
+        public System.Data.Entity.DbSet<ToDo.Models.List> Lists { get; set; }
+    }
+}
